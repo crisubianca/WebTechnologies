cfda596d45257e99f93dbc8f42ee1005da69768a
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2023-06-18 10:22:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     });
-  }  
+  } 
   else if (method === 'GET' && parsedUrl.pathname === '/login') {
     fs.readFile('./Views/login.html', (err, data) => {
       if (err) {
