55926bb2c253007f86f3b1b44883e1563a10520b
Cardaș Alexandra <alexandra.cardas1@gmail.com>
2023-06-22 00:49:54 +0300

    Login v1

--- Controllers/LoginController.js
+++ Controllers/LoginController.js
@@ -1,32 +1,32 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
 
-class LoginController {
-  static login = function(data) {
-    const { username, password } = data;
-    console.log('Username:', username);
-    console.log('Password:', password);
+// class LoginController {
+//   static login = function(data) {
+//     const { username, password } = data;
+//     console.log('Username:', username);
+//     console.log('Password:', password);
 
-    try {
-      const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
-      database.query(query, [username, password], (err, result) => {
-        if (err) {
-          console.error('Error executing MySQL query:', err);
-          return { status: 500, message: 'Internal Server Error' };
-        }
-        if (result.length === 0) {
-          return { status: 401, message: 'Invalid credentials' };
-        }
-      });
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-      return { status: 400, message: 'Bad Request' };
-    }
-    return { status: 200, message: 'Logged In Successfully' };
-  };
-}
+//     try {
+//       const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+//       database.query(query, [username, password], (err, result) => {
+//         if (err) {
+//           console.error('Error executing MySQL query:', err);
+//           return { status: 500, message: 'Internal Server Error' };
+//         }
+//         if (result.length === 0) {
+//           return { status: 401, message: 'Invalid credentials' };
+//         }
+//       });
+//     } catch (error) {
+//       console.error('Error parsing JSON:', error);
+//       return { status: 400, message: 'Bad Request' };
+//     }
+//     return { status: 200, message: 'Logged In Successfully' };
+//   };
+// }
 
-module.exports = LoginController;
+// module.exports = LoginController;
 
 
 
@@ -55,3 +55,21 @@ module.exports = LoginController;
 //     }
 // }
 // module.exports = LoginController;
+
+const { viewLogin } = require('../views/templates');
+
+function loginController(req, res) {
+    switch(req.method) {
+        case "GET" : 
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.end(viewLogin, 'utf8');
+            break;
+        default :
+            res.writeHead(405);
+            res.end();
+    }
+}
+
+module.exports = {
+    loginController
+}
--- Controllers/index.js
+++ Controllers/index.js
@@ -1,8 +1,12 @@
 const { publicController } = require("./public");
 const { registerController } = require("./register");
 const { registerUserController } = require("./registerUser");
+const { loginController } = require("./LoginController");
+const { loginUserController } = require("./loginUser");
 
 module.exports = {
+    loginController,
+    loginUserController,
     publicController,
     registerController,
     registerUserController
