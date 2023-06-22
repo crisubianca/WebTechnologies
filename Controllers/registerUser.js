44db42fbeefc8ef3ec8f27847ea6785ac1c8e781
Cardaș Alexandra <alexandra.cardas1@gmail.com>
2023-06-23 02:40:09 +0300

    Add register validation

--- Controllers/registerUser.js
+++ Controllers/registerUser.js
@@ -4,51 +4,57 @@ const bcrypt = require("bcrypt");
 
 let saltRounds = 10;
 async function registerUserController(req, res) {
-    switch(req.method) {
-        case 'POST' :
-            register(req, res);
-            break;
-        default :
-            res.writeHead(405);
-            res.end();
-    }
+  switch (req.method) {
+    case "POST":
+      register(req, res);
+      break;
+    default:
+      res.writeHead(405);
+      res.end();
+  }
 }
 
 async function register(req, res) {
-    try {
-        const body = await getPostData(req);
-        const { username, email, password } = body;
-        const user = {
-            username,
-            email,
-            password,
-        };
+  try {
+    const body = await getPostData(req);
+    const { username, email, password } = body;
+    const user = {
+      username,
+      email,
+      password,
+    };
 
-        user.password = await bcrypt.hash(password, saltRounds);
+    user.password = await bcrypt.hash(password, saltRounds);
 
-        console.log(
-            user.username,
-            user.email,
-            user.password
-        );
+    console.log(user.username, user.email, user.password);
 
+    userRepository
+      .findByUsername(user.username)
+      .then((result) => {
+        if (result) {
+          res.writeHead(409, { "Content-Type": "text/plain" });
+          console.log("an user with the same username already exists");
+        }
+      })
+      .catch((error) => {
         userRepository
-            .create(user)
-            .then((result) => {
-                res.writeHead(201, { "Content-Type": "text/plain" });
-                res.end("success");
-                // console.log("user added succesfully");
-            })
-            .catch((error) => {
-                res.writeHead(409, {"Content-Type" : "text/plain"});
-                res.end(error.constraint);
-                // console.log("user cannot be added into database");
-            });
-    } catch (error) {
-        console.log(error);
-    }
+          .create(user)
+          .then((result) => {
+            res.writeHead(201, { "Content-Type": "text/plain" });
+            res.end("success");
+            // console.log("user added succesfully");
+          })
+          .catch((error) => {
+            res.writeHead(409, { "Content-Type": "text/plain" });
+            res.end(error.constraint);
+            // console.log("user cannot be added into database");
+          });
+      });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
-    registerUserController,
+  registerUserController,
 };
